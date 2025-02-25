9bbe184c781b1b5efa7329896eef6352f224abe8
sarahnicoleboo <sarah.cantohyatt.5@my.csun.edu>
2025-02-24 16:57:43 -0800

    included all the baby java tests with consideration for skirting dead code elimination

--- first-benchmark/src/main/java/com/jenkov/MyBenchmark.java
+++ first-benchmark/src/main/java/com/jenkov/MyBenchmark.java
@@ -31,44 +31,73 @@
 
 package com.jenkov;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
+//import org.openjdk.jmh.annotations.Benchmark;
+//import org.openjdk.jmh.annotations.BenchmarkMode;
+//import org.openjdk.jmh.annotations.Mode;
+//import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class MyBenchmark {
-
-    static int[] doubleEachElement(int[] list) {
-        for (int i = 0; i < list.length; i++) {
-            list[i] = list[i] * 2;
-        }
-        return list;
+    
+    @State(Scope.Thread)
+    public static class ArrayModState {
+        public int[] list = {1,2,3};
     }
 
-    @Benchmark @BenchmarkMode(Mode.SingleShotTime) @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public void testArrayMod() {
-        /* This is a demo/sample template for building your JMH benchmarks. Edit as needed.
-        // Put your benchmark code here.
-        int a = 1;
-        int b = 2;
-        int sum = a + b;
-        */
-
-        int[] list = {1, 2, 3};
-        doubleEachElement(list);
-        for (int item : list) {
-            //System.out.println(item);
+    @Benchmark @BenchmarkMode(Mode.SingleShotTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public int[] testArrayMod(ArrayModState state) {
+        for (int i = 0; i < state.list.length; i++) {
+            state.list[i] = state.list[i] * 2;
         }
+        return state.list;
     }
 
-    @Benchmark @BenchmarkMode(Mode.SingleShotTime) @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public void testLoop() {
+    @Benchmark @BenchmarkMode(Mode.SingleShotTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void testLoop(Blackhole blackhole) {
         long sum = 0;
         for (long i = 0; i < 1_000_000_000L; i++) {
             sum += i;
         }
+        blackhole.consume(sum);
     }
-
+    
+    @State(Scope.Thread)
+    public static class PalindromeState {
+        String str = "hi";
+    }
+    
+    @Benchmark @BenchmarkMode(Mode.SingleShotTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void testPalindrome(PalindromeState state, Blackhole blackhole) {
+        int length = state.str.length();
+        boolean result = false;
+        for (int i = 0; i <= length; i++) {
+            if (length == 0 || length == 1) {
+                result = true;
+            } else if (state.str.charAt(i) != state.str.charAt(length-1)) {
+                result = false;
+            }
+            length--;
+        }
+        blackhole.consume(result);
+    }
+    
+    @Benchmark @BenchmarkMode(Mode.SingleShotTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public Boolean testPalindromeRecurisve(PalindromeState state) {
+        return isPalindrome(state.str);
+    }
+    
+    private Boolean isPalindrome(String str) {
+        if (str.length() == 1 || str.length() == 0) {
+            return true;
+        }
+        if (str.charAt(0) == str.charAt(str.length()-1)) {
+            return isPalindrome(str.substring(1,str.length()-1));
+        } else {
+            return false;
+        }
+    }
+    
 }
