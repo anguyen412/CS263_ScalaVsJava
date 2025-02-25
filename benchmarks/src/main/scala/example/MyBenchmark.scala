e49786756c01b6d410cff8c9af352f69da82ac31
sarahnicoleboo <sarah.cantohyatt.5@my.csun.edu>
2025-02-24 22:36:10 -0800

    working jmh with scala

---
+++ benchmarks/src/main/scala/example/MyBenchmark.scala
@@ -0,0 +1,19 @@
+package example
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.infra.Blackhole
+import java.util.concurrent.TimeUnit
+
+class MyBenchmark {
+
+  @Benchmark @OutputTimeUnit(TimeUnit.MILLISECONDS) @BenchmarkMode(Array(Mode.SingleShotTime)) 
+  def testMethod(blackHole: Blackhole): Double = {
+    val list: List[Int] = List.range(1, Integer.MAX_VALUE/100)
+    val sum: Double = list.sum
+    blackHole.consume(sum)
+    sum
+  }
+}
